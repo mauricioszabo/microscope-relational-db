2be4d0427d0a11f57f8c4d319471300ed0451438
Vinicius Higa <viniciushiga@gmail.com>
2017-04-28 10:57:52 -0300

    Upgrade to hsqldb 2.4.0

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [microscope "0.1.0"]
                  [com.mchange/c3p0 "0.9.5.1"]
                  [org.clojure/java.jdbc "0.6.1"]
-                 [org.hsqldb/hsqldb "2.3.4"]]
+                 [org.hsqldb/hsqldb "2.4.0"]]
 
   :profiles {:dev {:src-paths ["dev"]
                    :dependencies [[midje "1.8.3"]]
