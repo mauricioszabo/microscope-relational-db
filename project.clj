488de6cfd422842836ed5abd2b64edd428984728
Vinicius Higa <viniciushiga@gmail.com>
2017-04-28 10:56:28 -0300

    Upgrade to c3p0 0.9.5.2

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [microscope "0.1.0"]
-                 [com.mchange/c3p0 "0.9.2.1"]
+                 [com.mchange/c3p0 "0.9.5.1"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.hsqldb/hsqldb "2.3.4"]]
 
