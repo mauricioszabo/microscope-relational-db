1204456b99b127a6c88a1c02f4be29b5bddc9ce0
Rodolfo Liviero <rodolfoliviero@gmail.com>
2017-08-02 15:45:10 -0300

    moving org.hsqldb/hsqldb as test dependency (#4)
    * moving org.hsqldb/hsqldb as test dependency
    
    * bumped version
    
    * added changelog
    
    * bumped c3p0 and jdbc

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject microscope/relational-db "0.1.0"
+(defproject microscope/relational-db "0.1.1"
   :description "Microservice architecture for Clojure"
   :url "https://github.com/acessocard/microscope"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [microscope "0.1.3"]
-                 [com.mchange/c3p0 "0.9.5.1"]
-                 [org.clojure/java.jdbc "0.6.1"]
-                 [org.hsqldb/hsqldb "2.4.0"]]
+                 [com.mchange/c3p0 "0.9.5.2"]
+                 [org.clojure/java.jdbc "0.7.0"]]
 
   :profiles {:dev {:src-paths ["dev"]
-                   :dependencies [[midje "1.8.3"]]
+                   :dependencies [[org.hsqldb/hsqldb "2.4.0"]
+                                  [midje "1.8.3"]]
                    :plugins [[lein-midje "3.2.1"]]}})
